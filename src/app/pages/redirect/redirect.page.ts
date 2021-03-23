a23d3a1bfdfe9144010abe25940e3809cec5b7ff
rsbrum <rnsbrum@gmail.com>
2021-03-23 14:27:27 -0300

    new font, new logo

--- src/app/pages/redirect/redirect.page.ts
+++ src/app/pages/redirect/redirect.page.ts
@@ -12,4 +12,12 @@ export class RedirectPage implements OnInit {
   ngOnInit() {
   }
 
+  openApp() {
+    window.open('https://app.geradordeartes.com.br', '_system', 'location=yes'); return false;
+  }
+
+  openWpp() {
+    window.open('https://api.whatsapp.com/send?phone=+5554984091254&text=False conosco!', '_system', 'location=yes'); return false;
+  }
+
 }
